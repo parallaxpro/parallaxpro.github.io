500344ebe1fffd69aed36629ec5c2e298b75b3bc
vitaliybalayan <vitaliy.balayan@gmail.com>
2021-01-18 17:57:32 +0500

    courses

--- safety1st/app/js/app.js
+++ safety1st/app/js/app.js
@@ -1,9 +1,30 @@
-// import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i -D jquery)
-
+import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i -D jquery)
 // require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
 
-document.addEventListener('DOMContentLoaded', () => {
+$(document).ready(function() {
+	
+	$(document).mouseup(function (e) {
+		var container = $('.dropdown-btn');
+		var dropdown = $('.dropdown-list');
 
-	// Custom JS
+		if (container.has(e.target).length === 0 && dropdown.hasClass('show--block')) closeDropdown(dropdown);
+	});
+	
+});
 
+$(document).on('click', '.dropdown-btn', function(e) {
+	e.preventDefault();
+
+	var dropdown = $(this).children('.dropdown-list');
+
+	if (dropdown.hasClass('show--block')) closeDropdown(dropdown);
+	else dropdown.addClass('show--block');
 })
+
+function closeDropdown(block) {
+	block.addClass('close--block');
+	setTimeout(function() {
+		block.removeClass('show--block');
+		block.removeClass('close--block');
+	}, 250);
+}
