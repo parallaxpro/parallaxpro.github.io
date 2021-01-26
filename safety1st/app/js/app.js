b4612d52df53530f0d23180d40f2cd7dd3d6204c
vitaliybalayan <vitaliy.balayan@gmail.com>
2021-01-26 16:46:52 +0500

    release 1

--- safety1st/app/js/app.js
+++ safety1st/app/js/app.js
@@ -1,7 +1,11 @@
 import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i -D jquery)
+require('~/app/libs/jquery-ui.js');
+require('~/app/libs/jquery.fancybox.min.js');
 // require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
 
 $(document).ready(function() {
+
+	document.addEventListener('contextmenu', event => event.preventDefault());
 	
 	$(document).mouseup(function (e) {
 		var container = $('.dropdown-btn');
@@ -9,7 +13,31 @@ $(document).ready(function() {
 
 		if (container.has(e.target).length === 0 && dropdown.hasClass('show--block')) closeDropdown(dropdown);
 	});
+
+	$('.btn-add--user').on('click', function(e) {
+		e.preventDefault();
+
+		if ($('.user-add').is(':visible')) {
+			$('.user-add').slideUp();
+		} else {
+			$('.user-add').slideDown();
+		}
+	});
+
+	var mySwiper = new Swiper('.swiper-container', {
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	})
 	
+});
+
+$(function() {
+    $("#progressbar").progressbar({
+		value: $('#quiz-bar').data('value')
+	});
 });
 
 $(document).on('click', '.dropdown-btn', function(e) {
