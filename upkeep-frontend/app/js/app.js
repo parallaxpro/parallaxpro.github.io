9fd9b81328f1450b20ed62ed27ab17ec990f19bc
vitaliybalayan <vitaliy.balayan@gmail.com>
2021-06-25 17:58:31 +0500

    1

--- upkeep-frontend/app/js/app.js
+++ upkeep-frontend/app/js/app.js
@@ -1,18 +1,39 @@
 $(function () {
-    const swiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-    });
+	const swiper = new Swiper('.swiper-container', {
+			// Optional parameters
+			direction: 'horizontal',
+			loop: true,
+		
+			// If we need pagination
+			pagination: {
+				el: '.swiper-pagination',
+			},
+		
+			// Navigation arrows
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+	});
+
+	$('[data-cart_input]').on('keypress', function(event) {
+		return event.charCode >= 48 && event.charCode <= 57;
+	});
+
+	$('[data-cart_input]').blur(function() {
+		if ($(this).val().length < 1 || $(this).val() == 0) $(this).val(1);
+	});
+
+	$('.product-qty--plus').on('click', function() {
+		var input = $('input[data-cart_input='+ $(this).data('cart_input') +']');
+		var value = Number(input.val()) + 1;
+		input.val(value);
+	});
+
+	$('.product-qty--minus').on('click', function() {
+		var input = $('input[data-cart_input='+ $(this).data('cart_input') +']');
+		var value = Number(input.val());
+		var next_value = value - 1; 
+		if (value > 1) input.val(next_value);
+	});
 });
